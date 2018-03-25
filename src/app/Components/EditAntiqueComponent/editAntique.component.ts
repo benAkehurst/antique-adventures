695c728a0d97882eec92838a67dd113f526184a4
benAkehurst <benakehurst@hotmail.com>
2018-03-25 18:42:18 +0100

    bug with upload bars

--- src/app/Components/EditAntiqueComponent/editAntique.component.ts
+++ src/app/Components/EditAntiqueComponent/editAntique.component.ts
@@ -6,6 +6,8 @@ import { ActivatedRoute } from '@angular/router';
 import { FormGroup, AbstractControl, FormBuilder, Validators } from '@angular/forms';
 import { NgModel } from '@angular/forms';
 import swal from 'sweetalert';
+import { AngularFireStorage } from 'angularfire2/storage';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
     selector: 'app-edit-antique',
@@ -15,7 +17,7 @@ import swal from 'sweetalert';
 
 export class EditAntiqueComponent implements OnInit {
 
-    constructor(public dataService: DataService, private router: Router) { }
+    constructor(public dataService: DataService, private router: Router, private storage: AngularFireStorage) { }
 
     errors: any;
     isDataLoaded: Boolean = false;
@@ -23,10 +25,47 @@ export class EditAntiqueComponent implements OnInit {
     radioValue = { valueSigned: true, valueNotSigned: false };
     orderbydescending = true;
     itemId: String = '';
+    selectedImageFile = null;
+    selectedProvenanceFile = null;
+    toUpload = null;
+    uploadPercent: Observable<number>;
+    uploadPercentProv: Observable<number>;
+    imageUrl: Observable<string | null>;
+    provenanceImageUrl: Observable<string | null>;
 
     ngOnInit() {
         this.getItemId();
         this.populateForm();
+    }
+
+    public onImageFileSelected(event) {
+        this.selectedImageFile = event.target.files[0];
+    }
+
+    public onProvenanceFileSelected(event) {
+        this.selectedProvenanceFile = event.target.files[0];
+    }
+
+    public uploadFile() {
+        const file = this.selectedImageFile;
+        const filePath = 'antique_images/' + this.selectedImageFile.name;
+        const task = this.storage.upload(filePath, file);
+        this.uploadPercent = task.percentageChanges();
+        this.imageUrl = task.downloadURL();
+        this.imageUrl.subscribe(value => {
+            this.dataService.Antique.image = value;
+        });
+    }
+
+    public uploadProvenanceFile() {
+        const file = this.selectedProvenanceFile;
+        const filePath = 'antique_images/' + this.selectedProvenanceFile.name;
+        const task = this.storage.upload(filePath, file);
+        this.uploadPercentProv = task.percentageChanges();
+        this.provenanceImageUrl = task.downloadURL();
+        this.provenanceImageUrl.subscribe(value => {
+            this.dataService.Antique.provenanceImage = value;
+        });
     }
 
     public populateForm() {
@@ -37,7 +76,6 @@ export class EditAntiqueComponent implements OnInit {
             this.dataService.Antique.category = res.data.category;
             this.dataService.Antique.signed = res.data.signed;
             this.dataService.Antique.value = res.data.value;
-            this.dataService.Antique.image = res.data.image;
             this.dataService.Antique.description = res.data.description;
             this.dataService.Antique.condition = res.data.condition;
             this.dataService.Antique.width = res.data.width;
@@ -46,8 +84,9 @@ export class EditAntiqueComponent implements OnInit {
             this.dataService.Antique.material = res.data.material;
             this.dataService.Antique.location = res.data.location;
             this.dataService.Antique.provenance = res.data.provenance;
-            this.dataService.Antique.provenanceImage = res.data.provenanceImage;
             this.dataService.Antique.status = res.data.status;
+            this.dataService.Antique.image = res.data.image;
+            this.dataService.Antique.provenanceImage = res.data.provenanceImage;
         });
     }
 

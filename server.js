14a33e4950dbcd081eb16cc42ba30d7a38c97b3c
benAkehurst <benakehurst@hotmail.com>
2018-03-28 23:08:43 +0100

    v2

--- server.js
+++ server.js
@@ -41,21 +41,21 @@ app.use(express.static(path.join(__dirname, 'dist'))); // Point static path to d
 // Connect to DB with mongoose
 mongoose.Promise = global.Promise;
 // Local DB
-mongoose.connect("mongodb://localhost:27017/antiqueAdventuresDB", function (err) {
-    if (err) {
-        console.log("Error: " + err);
-    } else {
-        console.log("Connected to Database")
-    }
-});
-// Live DB
-// mongoose.connect(process.env.DB_CONNECT, function (err) {
+// mongoose.connect("mongodb://localhost:27017/antiqueAdventuresDB", function (err) {
 //     if (err) {
 //         console.log("Error: " + err);
 //     } else {
 //         console.log("Connected to Database")
 //     }
 // });
+// Live DB
+mongoose.connect(process.env.DB_CONNECT, function (err) {
+    if (err) {
+        console.log("Error: " + err);
+    } else {
+        console.log("Connected to Database")
+    }
+});
 
 // Catch all other routes and return the index file
 app.get('*', (req, res) => {
